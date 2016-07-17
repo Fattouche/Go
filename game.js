a0b17ad3d255849c3e21eb054df71917381dbdb2
Alex Fattouche <alex.fattouche@gmail.com>
2016-07-17 12:20:45 -0700

    Fixed the count score

--- game.js
+++ game.js
@@ -1,14 +1,28 @@
+/*
+
+Made by: Alex Fattouche 
+https://github.com/Fattouche/Go/
+
+This File is responsible for all the game functionality.
+The server will call specific functions like count score
+and checkMove throughout the game.
+
+*/
+
+
 module.exports = {
     PlayMove:PlayMove,
 	countScore:countScore
 }
 
+//This will check move and return a new board if valid move.
 function PlayMove(prevBoard,board,x,y,color){
 	var played=false;
 	newboard = [];
 	for(var i = 0; i < board.length; i++){
         newboard[i] = board[i].slice();
     }
+	//check if the move is valid
 	if(checkMove(prevBoard,newboard,x,y,color)){
 		
 		newboard[x][y]=color;
@@ -23,41 +37,45 @@ function PlayMove(prevBoard,board,x,y,color){
 	return state;
 }
 
-
+//Counts the score at the end of the game
 function countScore(board) {
   //for every empty spot on the board, search for white/black tokens and change accordingly
   var player1score=0;
   var player2score=0;
-  for (i = 0; i < board.length; i++) {
-    for (j = 0; j < board.length; j++) {
-		if(board[i][j]==4||board[i][j]==5){
-			board[i][j]=0;
+  
+  
+   for (i = 0; i < board.length; i++) {
+		for (j = 0; j < board.length; j++) {
+			if(board[i][j]==99||board[i][j]==8){
+					board[i][j]=0;
+			}
 		}
-	  }
-	}
+   }
+   //We want to keep checking the territory on the original array and not the updated array
+  var board2=JSON.parse(JSON.stringify(board));
+  var boardTemp=JSON.parse(JSON.stringify(board));
+  console.log(boardTemp);
+  
   for (i = 0; i < board.length; i++) {
     for (j = 0; j < board.length; j++) {
 		lastEncountered=0;
 		sameEncountered=true;
-	  if(board[i][j]==0){
-		board[i][j]=checkTerritory(board,i,j);
+	  if(board2[i][j]==0){
+		board[i][j]=checkTerritory(board2,i,j);
+		board2=JSON.parse(JSON.stringify(boardTemp));
 	  }
 	}
   }
 	for (i = 0; i < board.length; i++) {
 		for (j = 0; j < board.length; j++) {
-			if(board[i][j]==99){
-				board[i][j]=0;
-			}
-			if (board[i][j] == 1||board[i][j] == 4) {
+			if (board[i][j] == 1) {
 				player1score++;
 			}
-			if(board[i][j]==2||board[i][j] == 5){
+			if(board[i][j]==2){
 				player2score++;
 			}
 		}
 	}
-  
   if(player1score>player2score){
 	  var winner=1;
   }
@@ -79,27 +97,25 @@ function checkMove(PreviousBoard, NewBoard, x, y, color) {
   console.log("inside checkMove");
   if (NewBoard[x][y]!== 0) {
   	  console.log(NewBoard[x][y]);
-  	  console.log("inside 1");
     return false;
   }
   
   //check that the KO rule hasnt been broken
   NewBoard[x][y]=color;
   if (PreviousBoard.toString() === NewBoard.toString()) {
-  	console.log("inside 2");
   		NewBoard[x][y] = 0;
 		return false;
   }
   NewBoard[x][y]=0;
   //check that the token isnt suiciding
   if (isSurrounded(NewBoard, x, y, color)) {
-  	console.log("inside 3");
     return false;
   }
   console.log("inside 4");
   return true;
 }
 
+//remove any tokens that are now killed
 function cleanBoard(board) {
   for (i = 0; i < board.length; i++) {
     for (j = 0; j < board.length; j++) {
@@ -125,12 +141,32 @@ function cleanBoard(board) {
   
   return board;
 }
-
 lastEncountered=0;
 sameEncountered=true;
+//The main count score function
 function checkTerritory(board,x,y){
 	if(sameEncountered==false){
 		return 0;
+	}
+	if(x-1>=0){
+		if(board[x-1][y]==1){
+			if(lastEncountered==0){
+				lastEncountered=1;
+			}
+			if(lastEncountered==2){
+				sameEncountered=false;
+				return 0;
+			}
+		}
+		if(board[x-1][y]==2){
+			if(lastEncountered==0){
+				lastEncountered=2;
+			}
+			if(lastEncountered==1){
+				sameEncountered=false;
+				return 0;
+			}
+		}
 	}
 	if(y+1<board.length&&sameEncountered==true){
 		if(board[x][y+1]==1){
@@ -193,34 +229,28 @@ function checkTerritory(board,x,y){
 		}
 		
 	}
-	if(x-1>=0){
-		if(board[x-1][y]==1){
+	
+	if(x-1>=0&&sameEncountered==true){
+		if(board[x-1][y]==0){
+			board[x][y]=99;
+			lastEncountered=checkTerritory(board,x-1,y);
 			if(lastEncountered==0){
-				lastEncountered=1;
-			}
-			if(lastEncountered==2){
-				sameEncountered=false;
+				board[x][y]=8;
 				return 0;
 			}
-		}
-		if(board[x-1][y]==2){
-			if(lastEncountered==0){
-				lastEncountered=2;
-			}
-			if(lastEncountered==1){
-				sameEncountered=false;
-				return 0;
-			}
+			board[x][y]=8;
 		}
 	}
+	
 	if(y+1<board.length&&sameEncountered==true){
 		if(board[x][y+1]==0){
 			board[x][y]=99;
 			lastEncountered=checkTerritory(board,x,y+1);
 			if(lastEncountered==0){
+				board[x][y]=8;
 				return 0;
 			}
-			board[x][y]=0;
+			board[x][y]=8;
 		}
 	}
 	if(x+1<board.length&&sameEncountered==true){
@@ -228,9 +258,10 @@ function checkTerritory(board,x,y){
 			board[x][y]=99;
 			lastEncountered=checkTerritory(board,x+1,y);
 			if(lastEncountered==0){
+				board[x][y]=8;
 				return 0;
 			}
-			board[x][y]=0;
+			board[x][y]=8;
 		}
 	}
 	
@@ -239,22 +270,13 @@ function checkTerritory(board,x,y){
 			board[x][y]=99;
 			lastEncountered=checkTerritory(board,x,y-1);
 			if(lastEncountered==0){
+				board[x][y]=8;
 				return 0;
 			}
-			board[x][y]=0;
+			board[x][y]=8;
 		}
 	}
 	
-	if(x-1>=0&&sameEncountered==true){
-		if(board[x-1][y]==0){
-			board[x][y]=99;
-			lastEncountered=checkTerritory(board,x-1,y);
-			if(lastEncountered==0){
-				return 0;
-			}
-			board[x][y]=0;
-		}
-	}
 	return lastEncountered;
 }
 
@@ -282,6 +304,7 @@ function isSurrounded(board, x, y, color) {
     }
   }
 	
+//recursively check if all of your allies are surrounded
   if (x - 1 >= 0) {
     if (board[x - 1][y] == color) {
       board[x][y] = 99;
@@ -326,115 +349,4 @@ function isSurrounded(board, x, y, color) {
   }
   return true;
 }
-
-
-/*var assert = require('assert');
-
-describe('EndGame',function(){
-	
-	var arr = new Array(9);
-	for(i=0;i<9;i++){
-		arr[i]=new Array(9);
-	}
-		for(i=0;i<9;i++){
-			for(j=0;j<9;j++){
-				arr[i][j]=0;
-			}
-		}
-	var arr1 = new Array(9);
-	for(i=0;i<9;i++){
-		arr1[i]=new Array(9);
-	}
-		for(i=0;i<9;i++){
-			for(j=0;j<9;j++){
-				arr1[i][j]=0;
-			}
-		}
-		arr1[1][3]=1;
-		
-	arr[0][5]=1;
-	arr[1][5]=1;
-	arr[2][5]=1;
-	arr[3][5]=1;
-	arr[3][4]=1;
-	arr[2][3]=1;
-	arr[2][2]=1;
-	arr[2][1]=1;
-	arr[3][1]=2;
-	arr[3][0]=1;
-	arr[4][0]=1;
-	arr[5][0]=1;
-	arr[6][0]=1;
-	arr[6][1]=1;
-	arr[6][2]=1;
-	arr[3][4]=1;
-	arr[3][5]=1;
-	arr[3][6]=1;
-	arr[3][7]=1;
-	arr[3][8]=1;
-	arr[4][6]=1;
-	arr[4][7]=1;
-	arr[5][7]=1;
-	arr[5][8]=1;
-	arr[6][8]=1;
-	arr[6][4]=1;
-	
-	
-	
-	
-	arr[0][6]=2;
-	arr[1][6]=2;
-	arr[2][6]=2;
-	arr[2][7]=2;
-	arr[2][8]=2;
-	arr[7][0]=2;
-	arr[7][1]=2;
-	arr[7][2]=2;
-	arr[7][3]=2;
-	arr[6][3]=2;
-	arr[5][2]=2;
-	arr[5][1]=2;
-	arr[4][1]=2;
-	arr[3][2]=2;
-	arr[3][3]=2;
-	arr[4][3]=2;
-	arr[4][4]=2;
-	arr[4][5]=2;
-	arr[5][6]=2;
-	arr[6][6]=2;
-	arr[6][7]=2;
-	arr[7][7]=2;
-	arr[7][8]=2;
-	arr[2][0]=2;
-	arr[1][8]=2;
-	
-	/*
-	//console.log(isSurrounded(arr,3,5,1));
-	//console.log(arr);
-	//console.log(checkTerritory(arr,1,0));
-	//console.log(arr[1][0]);
-	//console.log(checkTerritory(arr,1,0));
-	//console.log(countScore(arr));
-	//console.log(checkMove(arr1,arr,1,7,1));
-	arr[1][7]=1;
-	arr[0][5]=2;
-	arr[8][4]=2;
-	arr=cleanBoard(arr);
-	console.log(arr);
-	state=PlayMove(arr1,arr,4,2,1);
-	console.log(state.Board);
-	console.log(arr);
-	if(checkMove(arr1,arr,0,7,1)){
-		arr[0][7]=1;
-		arr[0][8]=1;
-	}
-	console.log(arr);
-	console.log("\n");
-	//console.log(checkTerritory(arr,8,0));
-	arr=cleanBoard(arr);
-	console.log(arr);
-	console.log(countScore(arr));
-	console.log(arr);
-	
-});*/
 
