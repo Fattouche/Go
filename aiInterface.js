84a42b68ab4d7814cc4a32406a7ff753bb88aef4
claudiali666 <yufeng0827@gmail.com>
2016-07-20 12:58:11 -0700

    change different ai

--- aiInterface.js
+++ aiInterface.js
@@ -5,7 +5,7 @@ console.log("inside of aiInterface");
 function getRandomMove(size, board, lastMove, cb){
 	var options = {
 		host:'roberts.seng.uvic.ca',
-		path:'/ai/attackEnemy',
+		path:'/ai/maxLibs',
 		port: '30000',
 		method: 'POST',
 		headers:{
